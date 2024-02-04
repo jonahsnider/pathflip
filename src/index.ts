722a357bd1d93a8714c35d27a565787e3ee6cf4e
Jonah Snider <jonah@jonahsnider.com>
2024-02-04 02:43:10 -0800

    feat: automatically notify when there are updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
+import updateNotifier from 'update-notifier';
+import pkg from '../package.json' assert { type: 'json' };
+
 import { runCli } from './cli/cli.js';
+
+const notifier = updateNotifier({ pkg });
+
+notifier.notify({ defer: true });
 
 const success = await runCli();
 
