1b76db47de54422f6d2260ded7c75696c801fce6
Jonah Snider <jonah@jonahsnider.com>
2024-09-23 13:40:35 -0700

    fix: add warning about Node.js version on v20

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,3 +1,4 @@
+import { log } from '@clack/prompts';
 import { runIntro } from './steps/1/intro.js';
 import { scanForProjectFiles } from './steps/2/scan-for-project-files.js';
 import { selectPaths } from './steps/3/select-paths.js';
@@ -6,6 +7,10 @@ import { applyTransforms } from './steps/5/apply-transforms.js';
 
 export async function runCli(): Promise<void> {
 	runIntro();
+
+	if (process.version.startsWith('v20.')) {
+		log.warn('Pathflip requires Node.js v22 or later, you may experience issues with the current version of Node');
+	}
 
 	const projectFiles = await scanForProjectFiles();
 
